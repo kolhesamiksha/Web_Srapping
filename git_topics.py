683f9377940be1f7b6fa94649c2fb6344b3ce2fb
samiksha kolhe <73512374+kolhesamiksha@users.noreply.github.com>
2021-10-20 16:50:42 +0530

    Update git_topics.py

--- git_topics.py
+++ git_topics.py
@@ -4,21 +4,28 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
+#to get the URL
 url = "https://github.com/topics"
 response = requests.get(url)
 
+#Check the url is working or not
 if(response.status_code!=200):
     print("Error!")
 
+#To get the Content of Website
 texts = response.text
 bsoup = bs(texts,"html.parser")
 
+#To get the content of Topics Block
 div_class = bsoup.find_all("div",{"class":"py-4 border-bottom"})
 
+#For lists creation
 Title = []
 Description = []
+
 for i in range(len(div_class)):
-    #title for names of trending topics
+    
+    #To get the names of trending topics in a-tag
     title = div_class[i].find_all("a",{"class":"d-flex no-underline"})[0].text
     title = re.sub(r'[#]',"",title)
     title = title.strip().replace("   "," ")
@@ -26,18 +33,19 @@ for i in range(len(div_class)):
     Title.append(title[0])
 
 
-    #des for title's description
+    #To get the description of the topics
     des = title[1:2]
     des = [x.strip() for x in des]
     Description.extend(des)
 
+#Dict for further dataframe conversion
 Topics_dict = {
     "Title of Topics":Title,
     "Description":Description
 }
 
+#Dataframe 
 tp_frame = pd.DataFrame(Topics_dict)
 
+#Topics_Csv file
 Topics_csv = tp_frame.to_csv("Trending Topics Scrapping.csv")
-
-
